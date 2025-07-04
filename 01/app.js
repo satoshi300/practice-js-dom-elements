c8a0c55f0154f194b1c5389fb8a05cf053177b71
Michal Wasiak <michal.wasiak@hotmail.com>
2025-07-04 16:59:20 +1000

    Task 01 done

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,11 @@
 console.log('DOM');
+let sum = 0;
+const commentsItems = document.querySelector('.comments__item.comments__item--newest');
+if (commentsItems) {
+    console.log(commentsItems);
+    const dataInfoElements = commentsItems.querySelectorAll('[data-info]');
+    if (dataInfoElements) {
+            console.log('data-info wystepuje ' + dataInfoElements.length + ' razy');
+        }
+    }
+
