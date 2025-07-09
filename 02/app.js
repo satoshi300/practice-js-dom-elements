8e8a920c39e0e1239f6e0618864ab03fa0ffbf15
Michal Wasiak <michal.wasiak@hotmail.com>
2025-07-09 21:09:38 +1000

    Task 02 done

--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,15 @@
 console.log('DOM');
+const aList = document.querySelectorAll('a');
+if (aList) {
+    const dataUrl = document.querySelectorAll('[data-url]');
+    dataUrl.forEach(function (item) {
+        const link = item.getAttribute('data-url');
+        console.log(item)
+
+        item.setAttribute('href', link)
+    });
+
+
+}
+
+
